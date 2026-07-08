7db5c21ec03190b03eacd91602749f78a3185d71
Yash <yashwalke3434@gmail.com>
2026-07-08 23:00:08 +0530

    feat: add ThreatDetectionException with file and line tracking

---
+++ threatsentry/exceptions/exception.py
@@ -0,0 +1,30 @@
+import sys
+from threatsentry.logger import logger
+
+
+class ThreatDetectionException(Exception):
+    def __init__(self, error_message, error_details: sys):
+        self.error_message = error_message
+        _, _, exc_tb = error_details.exc_info()
+
+        self.lineno = exc_tb.tb_lineno
+        self.file_name = exc_tb.tb_frame.f_code.co_filename
+
+    def __str__(self):
+        return (
+            "Error occurred in script [{0}] "
+            "line [{1}] "
+            "message [{2}]".format(
+                self.file_name,
+                self.lineno,
+                str(self.error_message)
+            )
+        )
+
+
+if __name__ == '__main__':
+    try:
+        logger.info("Entering try block — testing exception handler")
+        a = 1 / 0
+    except Exception as e:
+        raise ThreatDetectionException(e, sys)
